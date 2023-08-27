3308c80a9aa0408a94a6a4d5b61352ba40d4bf1e
Azix <Azix1@outlook.fr>
2023-08-27 16:47:09 +0200

    optimisation code

--- IpBlocked.py
+++ IpBlocked.py
@@ -1,7 +1,11 @@
+from ipaddress import ip_network
 import pandas as pd
 import requests
 from collections import Counter
 from datetime import datetime
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 category_mapping = {
     1: "DNS Compromise",
@@ -29,6 +33,14 @@ category_mapping = {
     23: "IoT Targeted"
 }
 
+
+def is_subnet(entry: str):
+    try:
+        network = ip_network(entry, strict=False)
+        return network.num_addresses > 1
+    except ValueError:
+        return False
+
 class IpBlockedAPI:
     def __init__(self, api_key):
         self.api_key = api_key
@@ -39,72 +51,88 @@ class IpBlockedAPI:
             response.raise_for_status()
             return response.json()
         except requests.exceptions.RequestException as e:
-            print(f"Error occurred during API request: {e}")
+            logging.error(f"Error occurred during API request: {e}")
             return None
 
     def check_ip_abuse(self, ip):
         url = 'https://api.abuseipdb.com/api/v2/check'
-        params = {
-            'ipAddress': ip,
-            'maxAgeInDays': 90,
-            'verbose': True
-        }
-        headers = {
-            'Accept': 'application/json',
-            'Key': self.api_key
-        }
+        params = {'ipAddress': ip, 'maxAgeInDays': 90, 'verbose': True}
+        headers = {'Accept': 'application/json', 'Key': self.api_key}
         return self._make_request(url, headers, params)
 
     def process_file(self, filename):
+        df = self._load_file(filename)
+        ips_to_check = self._get_unchecked_ips(df)
+        for ip_or_subnet in ips_to_check:
+            self._process_ip_or_subnet(df, ip_or_subnet)
+        self._save_file(df, filename)
+
+    def _load_file(self, filename):
         try:
-            df = pd.read_excel(filename)
+            return pd.read_excel(filename)
         except FileNotFoundError:
-            df = pd.DataFrame(columns=["IP", "isPublic", "isWhitelisted", "abuseConfidenceScore", "countryCode", "usageType", "isp", "domain", "hostnames", "isTor", "totalReports", "numDistinctUsers", "lastReportedAt", "Most_Common_Comment", "Most_Common_Category", "IP_check"])
+            logging.error(f"File {filename} not found.")
+            exit(1)
 
-        ips_to_check = df[df['IP_check'].isna()]['IP'].dropna().unique()
+    def _get_unchecked_ips(self, df):
+        return df[df['IP_check'].isna()]['IP'].dropna().unique()
 
-        for ip in ips_to_check:
-            abuse_result = self.check_ip_abuse(ip)
-            if abuse_result:
-                data = abuse_result['data']
-                today_date = datetime.today().strftime('%Y-%m-%d')
-                
-                comments = [report["comment"] for report in data.get("reports", [])]
-                categories = [category for report in data.get("reports", []) for category in report.get("categories", [])]
+    def _process_ip_or_subnet(self, df, ip_or_subnet):
+        if is_subnet(ip_or_subnet):
+            logging.info(f"Checking subnet {ip_or_subnet}")
+        else:
+            self._update_dataframe_with_abuse_info(df, ip_or_subnet)
 
-                most_common_comment = Counter(comments).most_common(1)[0][0] if comments else "None"
-                most_common_category_num = Counter(categories).most_common(1)[0][0] if categories else "None"
-                most_common_category = category_mapping.get(most_common_category_num, "Unknown Category")
+    def _update_dataframe_with_abuse_info(self, df, ip):
+        abuse_result = self.check_ip_abuse(ip)
+        if abuse_result:
+            ip_data = self._prepare_ip_data(abuse_result)
+            self._update_or_append_dataframe(df, ip, ip_data)
 
-                ip_data = {
-                    "blocage_date": today_date,
-                    "IP": data.get("ipAddress"),
-                    "isWhitelisted": data.get("isWhitelisted"),
-                    "abuseConfidenceScore": data.get("abuseConfidenceScore"),
-                    "countryCode": data.get("countryCode"),
-                    "countryName": data.get("countryName"),
-                    "usageType": data.get("usageType"),
-                    "isp": data.get("isp"),
-                    "domain": data.get("domain"),
-                    "isTor": data.get("isTor"),
-                    "totalReports": data.get("totalReports"),
-                    "numDistinctUsers": data.get("numDistinctUsers"),
-                    "lastReportedAt": data.get("lastReportedAt"),
-                    "Most_Common_Comment": most_common_comment,
-                    "Most_Common_Category": most_common_category,
-                    "IP_check": True
-                }
+    def _prepare_ip_data(self, abuse_result):
+        data = abuse_result['data']
+        today_date = datetime.today().strftime('%Y-%m-%d')
 
-                existing_entry = df[df['IP'] == ip]
-                if existing_entry.empty:
-                    df = df.append(ip_data, ignore_index=True)
-                else:
-                    idx = existing_entry.index[0]
-                    for key, value in ip_data.items():
-                        df.at[idx, key] = value
+        comments = [report["comment"] for report in data.get("reports", [])]
+        categories = [category for report in data.get("reports", []) for category in report.get("categories", [])]
 
+        most_common_comment = Counter(comments).most_common(1)[0][0] if comments else "None"
+        most_common_category_num = Counter(categories).most_common(1)[0][0] if categories else "None"
+        most_common_category = category_mapping.get(most_common_category_num, "Unknown Category")
+
+        ip_data = {
+            "blocage_date": today_date,
+            "IP": data.get("ipAddress"),
+            "isWhitelisted": data.get("isWhitelisted"),
+            "abuseConfidenceScore": data.get("abuseConfidenceScore"),
+            "countryCode": data.get("countryCode"),
+            "countryName": data.get("countryName"),
+            "usageType": data.get("usageType"),
+            "isp": data.get("isp"),
+            "domain": data.get("domain"),
+            "isTor": data.get("isTor"),
+            "totalReports": data.get("totalReports"),
+            "numDistinctUsers": data.get("numDistinctUsers"),
+            "lastReportedAt": data.get("lastReportedAt"),
+            "Most_Common_Comment": most_common_comment,
+            "Most_Common_Category": most_common_category,
+            "IP_check": True
+        }
+        return ip_data
+
+    def _update_or_append_dataframe(self, df, ip, ip_data):
+        existing_entry = df[df['IP'] == ip]
+        if existing_entry.empty:
+            df = df.append(ip_data, ignore_index=True)
+        else:
+            idx = existing_entry.index[0]
+            for key, value in ip_data.items():
+                df.at[idx, key] = value
+
+    def _save_file(self, df, filename):
         df['IP_check'] = df['IP_check'].astype(bool)
         df.to_excel(filename, index=False)
+
 
 api_key = '79e4fd6e9853cd458888772dec91df33befc0880df1645fafebbffc594eea91c5140e1d9fd7009e9'  
 filename = 'ip_blocked.xlsx'
